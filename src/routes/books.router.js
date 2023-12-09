79effa376a5713df189052fc601897cdbe717c17
jshayk <hayk.ayvazyan@hexens.io>
2023-12-09 22:38:34 +0400

    helper Lilit

--- src/routes/books.router.js
+++ src/routes/books.router.js
@@ -7,36 +7,43 @@ const data = {
             title: "all the light we cannot see",
             author: "by anthony doerr",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus morbi eleifend enim, tristique",
+            largeDescription: "1"
         },
         {
             id: 2,
             title: "where the crawdads sing",
             author: "by delia owens",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus morbi eleifend enim, tristique ",
+            largeDescription: "2"
+
         },
         {
             id: 3,
             title: "where the crawdads sing",
             author: "by delia owens",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus morbi eleifend enim, tristique ",
+            largeDescription: "3"
         },
         {
             id: 4,
             title: "where the crawdads sing",
             author: "by delia owens",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus morbi eleifend enim, tristique ",
+            largeDescription: "4"
         },
         {
             id: 5,
             title: "where the crawdads sing",
             author: "by delia owens",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus morbi eleifend enim, tristique ",
+            largeDescription: "5"
         },
         {
             id: 6,
             title: "where the crawdads sing",
             author: "by delia owens",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus morbi eleifend enim, tristique ",
+            largeDescription: "6"
         },
     ],
     totalCount: 7
@@ -47,5 +54,20 @@ const router = express();
 router.get('/books', (req, res) => {
     res.send(data);
 });
+
+router.get('/books/:id', (req, res) => {
+    const { id } = req.params; 
+
+    if (!id) {
+        return res.status(403).send({message: "invalid id!"});
+    }
+    const book = data.list.find(book => book.id === Number(id));
+
+    if (!book) {
+        return res.status(200).send({message: "there is no book with this id!"})
+    }
+
+    res.status(200).send(book);
+})
 
 export default router;
